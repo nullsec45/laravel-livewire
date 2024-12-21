96ae331c572a419bb5d5525c1addfc79c8de7cd7
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-12-21 11:39:54 +0700

    change : fix controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\Home;
+use App\Livewire\About;
+use App\Livewire\Contact;
 use App\Livewire\Users\Show;
 use App\Livewire\Posts\Index;
 use Illuminate\Support\Facades\Route;
@@ -17,7 +19,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', Home::class)->name('home');
-Route::get('/about', Home::class)->name('about');
+Route::get('/home', Home::class)->name('home');
+Route::get('/about', About::class)->name('about');
+Route::get('/contact', Contact::class)->name('about');
 Route::get('/posts', Index::class)->name('posts.index');
 
 Route::get('users/{user}',Show::class)->name('users.show');
