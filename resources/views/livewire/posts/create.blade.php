8a74e62d91aa9b0c685bfe655836cfe022329b27
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-02 14:40:43 +0700

    modified : use x flash message & clean code create posts view.

--- resources/views/livewire/posts/create.blade.php
+++ resources/views/livewire/posts/create.blade.php
@@ -1,28 +1,31 @@
-<div class="card">
-   <div class="card-body">
-        <h5 class="card-title">New Post</h5>
-        <form wire:submit="save">
-            <div class="mb-4">
-                <label for="body" class="form-label">Title</label>
-                <input type="text" class="form-control" wire:model="title" class="form-control" id="title" rows="3">
-                @error('title')
-                    <div class="text-danger">
-                        {{$message}}
+<div>
+    <x-flash-message/>
+    <div class="card">
+        <div class="card-body">
+                <h5 class="card-title">New Post</h5>
+                <form wire:submit="save">
+                    <div class="mb-4">
+                        <label for="body" class="form-label">Title</label>
+                        <input type="text" class="form-control" wire:model="postForm.title" class="form-control" id="title" rows="3">
+                        @error('postForm.title')
+                            <div class="text-danger">
+                                {{$message}}
+                            </div>
+                        @enderror
                     </div>
-                @enderror
-            </div>
-            <div class="mb-4">
-                <label for="body" class="form-label">Body</label>
-                <textarea wire:model="body" class="form-control" id="body" rows="3"></textarea>
-                @error('body')
-                    <div class="text-danger">
-                        {{$message}}
+                    <div class="mb-4">
+                        <label for="body" class="form-label">Body</label>
+                        <textarea wire:model="postForm.body" class="form-control" id="body" rows="3"></textarea>
+                        @error('postForm.body')
+                            <div class="text-danger">
+                                {{$message}}
+                            </div>
+                        @enderror
                     </div>
-                @enderror
-            </div>
-            <button class="btn btn-success">
-                Save
-            </button>
-        </form>
-   </div>
+                    <button class="btn btn-success">
+                        Save
+                    </button>
+                </form>
+        </div>
+    </div>
 </div>
