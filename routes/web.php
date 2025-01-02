9755c2a208c0117db50e3a967b52ef12028dabfc
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-02 14:41:11 +0700

    modified : add route auth and middleware

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Livewire\Home;
+use App\Http\Controllers\LogoutController;
 use App\Livewire\About;
 use App\Livewire\Contact;
-use App\Livewire\Users\Show;
+use App\Livewire\Home;
+use App\Livewire\Login;
 use App\Livewire\Posts\Index;
+use App\Livewire\Users\Show;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,10 +20,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', Home::class)->name('home');
-Route::get('/home', Home::class)->name('home');
-Route::get('/about', About::class)->name('about');
-Route::get('/contact', Contact::class)->name('about');
-Route::get('/posts', Index::class)->name('posts.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/', Home::class)->name('home');
+    Route::get('/home', Home::class)->name('home');
+    Route::get('/about', About::class)->name('about');
+    Route::get('/contact', Contact::class)->name('about');
+    Route::get('/posts', Index::class)->name('posts.index');
 
-Route::get('users/{user}',Show::class)->name('users.show');
+    Route::get('users/{user}', Show::class)->name('users.show');
+    Route::post('/logout', LogoutController::class)->name('logout');
+});
+
+Route::get('/login', Login::class)->name('login')->middleware('guest');
