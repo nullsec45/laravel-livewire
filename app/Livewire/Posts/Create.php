6c8045e2775111f3188151d6ca9c3aa2240b1c5f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-02 14:38:03 +0700

    modified : move post form to PostForm model LiveWire

--- app/Livewire/Posts/Create.php
+++ app/Livewire/Posts/Create.php
@@ -2,29 +2,20 @@
 
 namespace App\Livewire\Posts;
 
-use App\Models\User;
+use App\Livewire\Forms\PostForm;
 use Livewire\Component;
 
 class Create extends Component
 {
-    #[\Livewire\Attributes\Rule(['required'])]
-    public string $title='';
+    public PostForm $postForm;
 
-    #[\Livewire\Attributes\Rule(['required'])]
-    public string $body='';
-
-    public function save(){
-       $user=User::find(1);
-       $validated=$this->validate();
-
-       $user->posts()->create($validated);
-
-       $this->reset();
+    public function save()
+    {
+        $this->postForm->store();
     }
 
     public function render()
     {
         return view('livewire.posts.create');
     }
-
 }
