c663cc703c7275e029027a717ef900a1a571a92f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-02 14:39:52 +0700

    new : layout for guest

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html  lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title ?? 'Page Title' }}</title>
+    @vite(['resources/js/app.js','resources/css/app.css'])
+  </head>
+  <body>
+    <x-navigation-menu/>
+
+   <main class="py-4">
+      <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-6">
+              {{$slot}}
+            </div>
+          </div>
+      </div>
+   </main>
+  </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</html>
