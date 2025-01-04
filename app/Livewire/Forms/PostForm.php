3989764bd01479df03afc4ef85db0f24f5ce6148
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-04 14:29:38 +0700

    return data $post

--- app/Livewire/Forms/PostForm.php
+++ app/Livewire/Forms/PostForm.php
@@ -2,28 +2,25 @@
 
 namespace App\Livewire\Forms;
 
+use Livewire\Form;
 use App\Models\User;
 use Livewire\Attributes\Rule;
-use Livewire\Form;
+use Illuminate\Support\Facades\Auth;
 
 class PostForm extends Form
 {
-    #[Rule(['required'])]
-    public string $title = '';
-
     #[Rule(['required'])]
     public string $body = '';
 
     public function store()
     {
-        $user = User::find(1);
-        $validated = $this->validate();
+        $post=Auth::user()->posts()->create($this->validate());
 
-        $user->posts()->create($validated);
 
-        //    session()->flash('message','Post created sucessfully');
         flash('Post created sucessfully', 'success');
 
         $this->reset();
+
+        return $post;
     }
 }
