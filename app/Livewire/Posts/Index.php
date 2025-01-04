ff250bb65a760696a4fd67bd101448dba2983571
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-04 14:31:02 +0700

    modified : dispatch event posts save

--- app/Livewire/Posts/Index.php
+++ app/Livewire/Posts/Index.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire\Posts;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[\Livewire\Attributes\On('postCreated')]
+    public function updatedList($post){
+
+    }
     public function render()
     {
-        return view('livewire.posts.index');
+        return view('livewire.posts.index',[
+            'posts' => Post::query()->with('user')->latest()->get()
+        ]);
     }
 }
