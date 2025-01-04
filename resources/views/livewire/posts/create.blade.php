77a2aee67709a103027f4a3005fe2eb20ea8cc3a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-04 14:33:58 +0700

    modified : remove titile in form post

--- resources/views/livewire/posts/create.blade.php
+++ resources/views/livewire/posts/create.blade.php
@@ -1,31 +1,21 @@
 <div>
-    <x-flash-message/>
-    <div class="card">
-        <div class="card-body">
-                <h5 class="card-title">New Post</h5>
-                <form wire:submit="save">
-                    <div class="mb-4">
-                        <label for="body" class="form-label">Title</label>
-                        <input type="text" class="form-control" wire:model="postForm.title" class="form-control" id="title" rows="3">
-                        @error('postForm.title')
-                            <div class="text-danger">
-                                {{$message}}
+            <x-flash-message/>
+            <div class="card">
+                <div class="card-body">
+                        <form wire:submit="save">   
+                            <div class="mb-2">
+                                <label for="body" class="visually-hidden">Body</label>
+                                <textarea placeholder="What's on your mind ?" wire:model="postForm.body" class="form-control" id="body" rows="3"></textarea>
+                                @error('postForm.body')
+                                    <div class="text-danger">
+                                        {{$message}}
+                                    </div>
+                                @enderror
                             </div>
-                        @enderror
-                    </div>
-                    <div class="mb-4">
-                        <label for="body" class="form-label">Body</label>
-                        <textarea wire:model="postForm.body" class="form-control" id="body" rows="3"></textarea>
-                        @error('postForm.body')
-                            <div class="text-danger">
-                                {{$message}}
-                            </div>
-                        @enderror
-                    </div>
-                    <button class="btn btn-success">
-                        Save
-                    </button>
-                </form>
-        </div>
-    </div>
+                            <button class="btn btn-success">
+                                Save
+                            </button>
+                        </form>
+                </div>
+            </div>
 </div>
