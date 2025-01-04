99ffe81363417ad9b40b0a3957dc0b7057a8746d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-04 14:34:16 +0700

    modified : add block component in index post

--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -1,3 +1,12 @@
 <div>
-    <livewire:posts.create/>
+    <div class="row">
+        <div class="col-lg-6">
+            <livewire:posts.create/>
+        </div>
+        <div class="col-lg-6">
+            @foreach($posts as $post)
+                <livewire:posts.block :post="$post" wire:key="{{$post->id}}"/>
+            @endforeach
+        </div>
+    </div>
 </div>
