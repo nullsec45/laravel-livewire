e4ec3afbf662dcb113148516d9a0ab46f8094f41
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-10 16:31:25 +0700

    modified: add placholder in timeline.

--- app/Livewire/Posts/Index.php
+++ app/Livewire/Posts/Index.php
@@ -4,7 +4,9 @@ namespace App\Livewire\Posts;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[\Livewire\Attributes\Lazy('Posts')]
 class Index extends Component
 {
     #[\Livewire\Attributes\On('postCreated')]
@@ -16,5 +18,9 @@ class Index extends Component
         return view('livewire.posts.index',[
             'posts' => Post::query()->with('user')->latest()->get()
         ]);
+    }
+
+    public function placeholder(){
+        return view('livewire.posts.placeholder');
     }
 }
