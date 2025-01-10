76053c630e566bf088acf966b2fade34c8a6177d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-10 16:34:56 +0700

    modified : change posts block to timeline page.

--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -1,12 +1,5 @@
 <div>
-    <div class="row">
-        <div class="col-lg-6">
-            <livewire:posts.create/>
-        </div>
-        <div class="col-lg-6">
-            @foreach($posts as $post)
-                <livewire:posts.block :post="$post" wire:key="{{$post->id}}"/>
-            @endforeach
-        </div>
-    </div>
+    @foreach($posts as $post)
+        <livewire:posts.block :post="$post" wire:key="{{$post->id}}" lazy/>
+    @endforeach
 </div>
